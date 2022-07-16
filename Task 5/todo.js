a4641273fbd96d6baf87512ccfa2bcbc765165b1
Lumenfire <sharon.ipsen1@gmail.com>
2022-07-16 15:19:18 +1000

    Finished task 5

---
+++ Task 5/todo.js
@@ -0,0 +1,12 @@
+let vue = new Vue({
+    el: '#app',
+    data: {
+        todos: [
+            { title: 'Task1', checked: true },
+            { title: 'Task2', checked: true },
+            { title: 'Task3', checked: true },
+            { title: 'Task4', checked: true },
+            { title: 'Task5', checked: false },
+        ],
+    },
+});
