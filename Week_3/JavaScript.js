ca7a4385dde9d8874e9fea6d638b0b91785d2f46
Lumenfire <sharon.ipsen1@gmail.com>
2022-07-27 20:02:22 +1000

    Week 3 task 2

--- Week_3/JavaScript.js
+++ Week_3/JavaScript.js
@@ -25,3 +25,51 @@ function myFunction() {
     meal.replace(/CHOCOLATE/i,"cake");
     console.log(meal);
   }
+
+  let fruits = ['Apple', 'Pear', 'Watermelon', 'Strawberry'];
+  console.log(fruits);
+  console.log(fruits.find((fruit) => fruit === 'Pear'));
+  console.log(fruits.find((fruit) => fruit === 'Banana'));
+
+  console.log(fruits.findIndex((fruit) => fruit === 'Pear'));
+  console.log(fruits.findIndex((fruit) => fruit === 'Banana'));
+  console.log(fruits.findIndex((fruit) => fruit === 'Watermelon'));
+
+  console.log(fruits.push('Peach'));
+  console.log(fruits)
+  console.log(fruits.findIndex((fruit) => fruit === 'Peach'));
+
+  console.log(fruits.pop());
+  console.log(fruits)
+  console.log(fruits.findIndex((fruit) => fruit === 'Peach'));
+
+  let numbers = [6, 9, 1, 7, 3];
+  console.log(numbers);
+  console.log(numbers.sort());
+  console.log(numbers);
+
+  console.log(fruits);
+  console.log(fruits.slice(1));
+  console.log(fruits.slice(1, 3));
+  console.log(fruits.slice(-1));
+
+  fruits.forEach((fruit) => console.log(fruit));
+  numbers.forEach((number) => console.log(number));
+
+  console.log(fruits.includes("Mango"));
+  console.log(fruits.includes("Apple"));
+  console.log(numbers.includes(3));
+  console.log(fruits.includes("Apple", 2));
+
+  let length = fruits.length;
+  console.log(length);
+
+  console.log(numbers.unshift(8));
+  console.log(numbers);
+
+  let x = 2.365;
+  console.log(x);
+  console.log(x.toExponential(2));
+  console.log(x.toPrecision(6));
+
+
